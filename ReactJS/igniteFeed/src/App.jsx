50b8ebc7038d3ac24f53cc1047624c5800f4343d
Rafael Santos Salgado <64908399+RafaelSantos1234@users.noreply.github.com>
2024-05-09 20:26:20 -0300

    igniteFeed-inicio
    Começando o primeiro projeto do curso de ReactJs da Rocketseat.

---
+++ ReactJS/igniteFeed/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+import './global.css'
+import { Header } from './components/Header'
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App
