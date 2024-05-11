c71840e9c8ec578a0633297b3097e46aa74bdad5
Rafael Santos Salgado <64908399+RafaelSantos1234@users.noreply.github.com>
2024-05-10 21:23:49 -0300

    componente Sidebar
    componente sidebar onde abriga as informações do usuário.

--- ReactJS/igniteFeed/src/App.jsx
+++ ReactJS/igniteFeed/src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 
 
 import './global.css'
+import styles from './App.module.css'
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 
 function App() {
 
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+
+      <Sidebar/>
+
+        <main>
+          posts
+        </main>
+
+      </div>
     </div>
   )
 }
---
+++ ReactJS/igniteFeed/src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import {PencilLine} from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1633356122544-f134324a6cee?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
+            <div className={styles.profile}>
+
+                <img src="https://avatars.githubusercontent.com/u/168784200?v=4" alt="Uma foto do meu rosto" />
+
+                <strong>Rafael Santos</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu Perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
